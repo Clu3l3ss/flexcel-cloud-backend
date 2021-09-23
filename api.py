e0a61a49a8361a614401338201087084a6d6ce9f
DGuan64 <69877310+DGuan64@users.noreply.github.com>
2021-09-22 22:57:43 -0500

    api mysql

--- api.py
+++ api.py
@@ -9,24 +9,28 @@ app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+#db.create_all() Create only on first run
 
 class FlowModel(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	flow = db.Column(MutableList.as_mutable(PickleType), default=[], nullable=False)
+	id = db.Column(db.String(100), primary_key=True)
+	flow = db.Column(db.String(100), nullable=False)
 
 	def __repr__(self):
 		return f"Flow(ID = {id})"
 
 flow_put_args = reqparse.RequestParser()
-flow_put_args.add_argument("flow", type=list, help="Flow is required", required=True)
+flow_put_args.add_argument("flow", type=str, help="Flow is required", required=True)
 
 flow_update_args = reqparse.RequestParser()
-flow_update_args.add_argument("flow", type=list, help="Flow is required", required = True)
+flow_update_args.add_argument("flow", type=str, help="Flow is required", required = True)
 
 resource_fields = {
-	'id': fields.Integer,
-	'flow': fields.List,
+	'id': fields.String,
+	'flow': fields.String
 }
+
+#try abort? 36
+#statuses
 
 class Flowdata(Resource):
 	@marshal_with(resource_fields)
@@ -42,8 +46,7 @@ class Flowdata(Resource):
 		result = FlowModel.query.filter_by(id=flow_id).first()
 		if result:
 			abort(409, message="Flow already stored.")
-
-		flow = FlowModel(id=flow_id, flow = args['flow'])
+		flow = FlowModel(id=flow_id, flow=args['flow'])
 		db.session.add(flow)
 		db.session.commit()
 		return flow, 201
@@ -54,16 +57,22 @@ class Flowdata(Resource):
 		result = FlowModel.query.filter_by(id=flow_id).first()
 		if not result:
 			abort(404, message="Flow doesn't exist, cannot update.")
-
 		if args['flow']:
 			result.flow = args['flow']
-
 		db.session.commit()
-
 		return result
+	
+	@marshal_with(resource_fields)
+	def delete(self, flow_id):
+		result = FlowModel.query.filter_by(id=flow_id).first()
+		if not result:
+			abort(404, message="Flow doesn't exist, cannot update.")
+		flow = FlowModel(id=flow_id, flow=result.flow)
+		db.session.delete(flow)
+		db.session.commit()
+		return flow, 201
 
-
-api.add_resource(Flowdata, "/flow/<int:user_id>")
+api.add_resource(Flowdata, "/flow/<string:user_id>")
 
 if __name__ == "__main__":
 	app.run(debug=True)
