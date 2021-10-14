09f75d8d04254e22b82f87da91dc8a502992f479
DGuan64 <69877310+DGuan64@users.noreply.github.com>
2021-10-13 21:17:21 -0500

    sufhwiuahefuawyehfwa

--- api.py
+++ api.py
@@ -41,30 +41,54 @@ app = Flask(__name__)
             #json.dump(args['value'], json_file)
         #return '', 200
 
-@app.route('/storage/<string:user_id>', methods=['GET', 'PUT'])
-def storage(user_id):
+@app.route('/storage/<string:user_id>/<string:folder>', methods=['GET', 'PUT'])
+def storage(user_id, folder):
     # GET request
     if request.method == 'GET':
-        args = request.args.get('type')
-        PATH = path + user_id + "/" + args + '.json'
-        if os.path.isfile(PATH) == False:
-            abort(404, message="Could not find data.")
-        with open(PATH) as f:
-            return json.load(f), 200
+        if folder == "None":
+            args = request.args.get('type')
+            PATH = path + user_id + "/" + args + '.json'
+            if os.path.isfile(PATH) == False:
+                abort(404, message="Could not find data.")
+            with open(PATH) as f:
+                return json.load(f), 200
+        else:
+            args = request.args.get('type')
+            PATH = path + user_id + "/" + folder + "/" + args + '.json'
+            if os.path.isfile(PATH) == False:
+                abort(404, message="Could not find data.")
+            with open(PATH) as f:
+                return json.load(f), 200
     # PUT request
     if request.method == 'PUT':
-        args = request.get_json()
-        PATH = path + user_id + "/" + args['type'] + '.json'
-        if os.path.exists('storage') == False:
-            os.mkdir('storage')
-        if os.path.exists(path + user_id) == False:
-            os.mkdir(path + user_id)
-        if os.path.isfile(PATH) == False:
-            with open(PATH, 'w') as fp:
-                pass
-        with open(PATH, 'w') as json_file:
-            json.dump(args['value'], json_file)
-        return {"TEST" : "000"}, 200
+        if folder == "None":
+            args = request.get_json()
+            PATH = path + user_id + "/" + args['type'] + '.json'
+            if os.path.exists('storage') == False:
+                os.mkdir('storage')
+            if os.path.exists(path + user_id) == False:
+                os.mkdir(path + user_id)
+            if os.path.isfile(PATH) == False:
+                with open(PATH, 'w') as fp:
+                    pass
+            with open(PATH, 'w') as json_file:
+                json.dump(args['value'], json_file)
+            return {"Done" : "Done"}, 200
+        else:
+            args = request.get_json()
+            PATH = path + user_id + "/" + folder + "/" + args + '.json'
+            if os.path.exists('storage') == False:
+                os.mkdir('storage')
+            if os.path.exists(path + user_id) == False:
+                os.mkdir(path + user_id)
+            if os.path.exists(path + user_id + "/" + folder) == False:
+                os.mkdir(path + user_id + "/" + folder)
+            if os.path.isfile(PATH) == False:
+                with open(PATH, 'w') as fp:
+                    pass
+            with open(PATH, 'w') as json_file:
+                json.dump(args['value'], json_file)
+            return {"Done" : "Done"}, 200
 
 #api.add_resource(storage, "/storage/<string:user_id>")
 
